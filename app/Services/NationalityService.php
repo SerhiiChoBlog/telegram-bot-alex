196a9dac290392f1ccaed36b2714530fed5c9afc
SerhiiCho <serhiicho@protonmail.com>
2021-12-24 09:53:54 +0200

    Added HTTP request to NationalityService.php

--- app/Services/NationalityService.php
+++ app/Services/NationalityService.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class NationalityService implements Service
 {
-    public function handle(): mixed
+    private const URL = 'https://api.nationalize.io/?name=';
+
+    public function __construct(private string $first_name)
     {
-        return;
+    }
+
+    public function handle(): array
+    {
+        return Http::get(self::URL . $this->first_name)->json();
     }
 }
-
