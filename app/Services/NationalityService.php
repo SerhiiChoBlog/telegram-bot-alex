ed322ff2383cea4772964b97221bc0b484854928
SerhiiCho <serhiicho@protonmail.com>
2021-12-24 09:51:56 +0200

    Added empty services

---
+++ app/Services/NationalityService.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+class NationalityService implements Service
+{
+    public function handle(): mixed
+    {
+        return;
+    }
+}
+
---
+++ app/Services/QrCodeService.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+class QrCodeService implements Service
+{
+    public function handle(): mixed
+    {
+        return;
+    }
+}
+
